53129a86e3ae330ef264117d4fe2e08cf4a79a9f
Marla Pablianny <pabliannymarla@gmail.com>
2021-10-08 11:55:29 -0300

    Finalizando .env

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-console.log(process.env.DBHOST)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
