0b5943aeb5b57686a135623843de9daa69681d88
Marla Pablianny <pabliannymarla@gmail.com>
2021-10-08 11:43:25 -0300

    DBHOST

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,11 +15,11 @@ import { boolean } from 'boolean';
         "type": 'mysql', 
         "host": process.env.TYPEORM_HOST,
         "port": parseInt(process.env.TYPEORM_PORT), 
-        "username": process.env.TYPEORM_USERNAME, 
-        "password": process.env.TYPEORM_PASSWORD, 
-        "database": process.env.TYPEORM_DATABASE, 
-        "entities": ["dist/**/*.entity{.ts,.js}"],
-        "synchronize": boolean(process.env.TYPEORM_SYNCHRONIZE)
+        //"username": process.env.TYPEORM_USERNAME, 
+        //"password": process.env.TYPEORM_PASSWORD, 
+        //"database": process.env.TYPEORM_DATABASE, 
+        //"entities": ["dist/**/*.entity{.ts,.js}"],
+        //"synchronize": boolean(process.env.TYPEORM_SYNCHRONIZE)
       })
     }),
     MedicamentosModule,
